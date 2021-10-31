85fd6a30d0b9a439f298c4c3f07bc4f0fa600d52
howard.reith@gmail.com <howard.reith@gmail.com>
2021-10-31 14:49:31 -0400

    Change performer to artist in data model

--- models/accompanimentModel.js
+++ models/accompanimentModel.js
@@ -8,7 +8,7 @@ const ratingSchema = new mongoose.Schema({
 const accompanimentModel = new mongoose.Schema({
   songId: { type: mongoose.Schema.Types.ObjectId, ref: 'Song' },
   url: { type: String, required: true },
-  performer: { type: String, required: false },
+  artist: { type: String, required: false },
   dateCreated: { type: Date, required: false },
   price: { type: String, required: false },
   key: { type: String, required: false },
