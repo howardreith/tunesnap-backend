c91a95983da3f15b9e812c031807c4ff9353dad0
howard.reith@gmail.com <howard.reith@gmail.com>
2021-11-04 13:00:08 -0400

    Update jest. Properly mock aws so the test does not create an accompaniment in the real box.

--- utils/s3Helpers.js
+++ utils/s3Helpers.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const Bucket = process.env.AWS_BUCKET_NAME;
-const accessKeyId = process.env.AWS_ACCESS_KEY_IC;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_ACCESS_KEY_SECRET;
 
 const s3 = new AWS.S3({
--- utils/s3Helpers.test.js
+++ utils/s3Helpers.test.js
@@ -1,27 +1,32 @@
+import AWS from 'aws-sdk';
 import { getFileAndAddItToResponse, uploadFile } from './s3Helpers';
 
-const mS3Instance = {
-  upload: jest.fn().mockReturnThis(),
-  promise: jest.fn(),
-  getObject: jest.fn().mockReturnThis(),
-};
-
-jest.mock('aws-sdk', () => ({ S3: jest.fn(() => mS3Instance) }));
-
 describe('s3Helpers', () => {
+  let mockUpload;
+
+  beforeEach(() => {
+    mockUpload = jest.spyOn(AWS.S3.prototype, 'upload');
+    mockUpload.mockReturnValue({ promise: jest.fn() });
+  });
+
   describe('uploadFile', () => {
     it('should upload the file to s3', async () => {
       // eslint-disable-next-line global-require
       const file = { path: '\\tmp\\dbfe4d05fe7ff846acf73ea416edeefe', filename: 'aNicefilename', mimetype: 'mp3' };
-      const result = await uploadFile(file);
+      await uploadFile(file);
       const expected = {
-        ETag: '"3d9b3f65480ed094c0920feefec9c22a"',
-        Location: 'https://s3.amazonaws.com/media.tunesnap/aNicefilename',
-        key: 'aNicefilename',
         Key: 'aNicefilename',
         Bucket: 'media.tunesnap',
       };
-      expect(result).toEqual(expected);
+      expect(mockUpload).toHaveBeenCalledWith(expect.objectContaining(expected));
+      // Not going to test a return value as that would basically be testing my mock,
+      // but it should look like the following:
+      //   const expected = {
+      //     ETag: '"3d9b3f65480ed094c0920feefec9c22a"',
+      //     Location: 'https://s3.amazonaws.com/media.tunesnap/aNicefilename',
+      //     Key: 'aNicefilename',
+      //     Bucket: 'media.tunesnap',
+      //   };
     });
   });
 
